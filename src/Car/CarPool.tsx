be7e80437197d3a8205e9d99cd06377115d15b79
Harry Billings <harry@froogo.co.uk>
2021-02-03 17:41:00 +0000

    Added different car colours
    Added unique car colours based on the original board game.

--- src/Car/CarPool.tsx
+++ src/Car/CarPool.tsx
@@ -8,27 +8,27 @@ function CarPool() {
             <div className="Title">Car park</div>
             <div className="DoubleWidth">
                 <div>
-                    <CarSlot colour="#00FF00" truck={false}/>
-                    <CarSlot colour="#00FF00" truck={false}/>
-                    <CarSlot colour="#00FF00" truck={false}/>
-                    <CarSlot colour="#00FF00" truck={false}/>
-                    <CarSlot colour="#00FF00" truck={false}/>
-                    <CarSlot colour="#00FF00" truck={false}/>
+                    <CarSlot colour="#F60A05" truck={false}/>
+                    <CarSlot colour="#F16A17" truck={false}/>
+                    <CarSlot colour="#00966E" truck={false}/>
+                    <CarSlot colour="#27B8E7" truck={false}/>
+                    <CarSlot colour="#444D1C" truck={false}/>
+                    <CarSlot colour="#47B78D" truck={false}/>
                 </div>
                 <div>
-                    <CarSlot colour="#00FF00" truck={false}/>
-                    <CarSlot colour="#00FF00" truck={false}/>
-                    <CarSlot colour="#00FF00" truck={false}/>
-                    <CarSlot colour="#00FF00" truck={false}/>
-                    <CarSlot colour="#00FF00" truck={false}/>
-                    <CarSlot colour="#00FF00" truck={false}/>
+                    <CarSlot colour="#E48E8D" truck={false}/>
+                    <CarSlot colour="#48464B" truck={false}/>
+                    <CarSlot colour="#C4A783" truck={false}/>
+                    <CarSlot colour="#3652D7" truck={false}/>
+                    <CarSlot colour="#E0D458" truck={false}/>
+                    <CarSlot colour="#86463A" truck={false}/>
                 </div>
             </div>
             <div className="TruckSlots">
-                <CarSlot colour="#00FF00" truck={true}/>
-                <CarSlot colour="#00FF00" truck={true}/>
-                <CarSlot colour="#00FF00" truck={true}/>
-                <CarSlot colour="#00FF00" truck={true}/>
+                <CarSlot colour="#BC9FD9" truck={true}/>
+                <CarSlot colour="#004BDE" truck={true}/>
+                <CarSlot colour="#02D2BE" truck={true}/>
+                <CarSlot colour="#FBA70D" truck={true}/>
             </div>
         </section>
     );
