6616d1d1e4eea8eb6108745ed54735836ba8482f
Harry Billings <harry@froogo.co.uk>
2021-02-03 18:56:46 +0000

    Fixed bad attribute names on SVG
    Fixed regular HTML attribute names on SVG from Tabler Icons as React has different attribute names.

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -50,8 +50,8 @@ function Tile(props: {exit?: boolean}) {
             <div className="Tile">
                 { props.exit &&
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-narrow-right"
-                         width="3vw" height="3vw" viewBox="0 0 24 24" stroke-width="1.5" stroke="var(--background)" fill="none"
-                         stroke-linecap="round" stroke-linejoin="round">
+                         width="3vw" height="3vw" viewBox="0 0 24 24" strokeWidth="1.5" stroke="var(--background)" fill="none"
+                         strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <line x1="5" y1="12" x2="19" y2="12"/>
                         <line x1="15" y1="16" x2="19" y2="12"/>
