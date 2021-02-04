00a1bf7e7d2a9c2f417b46dc7efa8ccb8ff7aae1
Harry Billings <harry@froogo.co.uk>
2021-02-04 19:26:08 +0000

    Added source code link to navbar
    Added a link to the GitHub repo on the navbar.

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -7,6 +7,8 @@ function Navbar() {
             <div className="Title">Rush Hour Solver</div>
             {/* eslint-disable-next-line */}
             <div className="Credit">by <a href="https://froogo.co.uk/" target="_blank">Froogo</a></div>
+            {/* eslint-disable-next-line */}
+            <div className="Source"><a href="https://github.com/VolticFroogo/rush-hour-web" target="_blank">Check out the source code on GitHub</a></div>
         </div>
     );
 }
