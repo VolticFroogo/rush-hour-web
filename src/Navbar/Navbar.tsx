37a8302a0c52b44b809dbe1b2af7a01df0411d75
Harry <harry@froogo.co.uk>
2025-04-22 11:19:15 +0100

    Add link click tracking

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -6,9 +6,9 @@ function Navbar() {
         <div className="Navbar">
             <div className="Title">Rush Hour Solver</div>
             {/* eslint-disable-next-line */}
-            <div className="Credit">by <a href="https://froogo.co.uk/" target="_blank">Froogo</a></div>
+            <div className="Credit">by <a href="https://froogo.co.uk/" target="_blank" data-umami-event="click-link-froogo">Harry</a></div>
             {/* eslint-disable-next-line */}
-            <div className="Source"><a href="https://github.com/VolticFroogo/rush-hour-web" target="_blank">Check out the source code on GitHub</a></div>
+            <div className="Source"><a href="https://github.com/VolticFroogo/rush-hour-web" target="_blank" data-umami-event="click-link-github">Check out the source code on GitHub</a></div>
         </div>
     );
 }
